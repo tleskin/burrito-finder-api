390884f33c1355cd15002f96b2fdef70f6c2c689
Thomas Leskin <tleskin@gmail.com>
2015-06-16 14:54:11 -0600

    Pulled in burrito locations through Yelp API

---
+++ app/services/yelp_service.rb
@@ -0,0 +1,9 @@
+class YelpService
+
+  def burritos(lat, lon)
+    params = { term: 'burrito', limit: 10 }
+    coordinates = { latitude: lat, longitude: lon }
+    Yelp.client.search_by_coordinates(coordinates, params).businesses
+  end
+
+end
