f80ebc6645d09bdacbea473046934139c458c3b8
Thomas Leskin <tleskin@gmail.com>
2015-06-16 13:54:36 -0600

    Fix CSS loading order, add favorites, fix authorization permission

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,6 @@
 class MapsController < ApplicationController
+  before_action :authorize!
+  
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root to: "home#index"
-  resources :maps 
+  resources :maps
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  resources :favorites
 end
