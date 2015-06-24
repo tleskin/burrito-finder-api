56c08eb93fc9d8e35436f9e7318d3a2264f84f57
Thomas Leskin <tleskin@gmail.com>
2015-06-24 11:26:38 -0600

    Add controller tests for about page and json post request to find_burritos

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -3,16 +3,36 @@ require "helpers"
 
 RSpec.describe MapsController, type: :controller do
 
+  before(:each) do
+    @user = User.create({id: 1,
+                        provider: "facebook",
+                        uid: "11111111111111111",
+                        name: "Jorge Snow",
+                        email: "jorge@turing.io"})
+  end
+
   describe "GET #index" do
     it "returns http success" do
-      user = User.create({id: 1,
-                          provider: "facebook",
-                          uid: "11111111111111111",
-                          name: "Jorge Snow",
-                          email: "jorge@turing.io"})
-
-      get :index, nil, { user_id: user.id}
+      get :index, nil, { user_id: @user.id}
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about, nil, { user_id: @user.id}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #burritos" do
+    it "returns http success" do
+      VCR.use_cassette("find_burritos") do
+        xhr :post, :find_burritos, {lat:39.749964,  lon: -105.000012}, { user_id: @user.id}
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
 end
